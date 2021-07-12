dd1840dd664bb91ccaa258e832ad4d1b171adaa2
MrRedSea <87283299+MrRedSea@users.noreply.github.com>
2021-07-12 14:57:40 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+<script>
+let int=0;
+number = int;
+let min=0;
+let max=50;
+
+
+    document.getElementById( 'plus' ).addEventListener('click',fuction(){
+        document.getElementById('.number').innerHTML = Number
+        if (number < max){number = number + 1};
+    } );
+
+    document.getElementById( 'minus' ).addAventListener('click',fuction(){
+        document.getElementById('.number').innerHTML = Number
+        if(number > min){number = number - 1};
+    }  );
+
+</script>
